e5c20ac8d3193020960ded0d0ff249fec2731ff3
kynlux <kynlux@yandex.com>
2025-02-04 21:47:06 +0500

    v0.2.0-alpha

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 #include <windows.h>
 
 int main() {
@@ -13,7 +14,9 @@ int main() {
   int record_time;
   clock_t start_time, end_time;
   int score_v2 = 0;
+  int score_v3 = 0;
   int combo = 0;
+  int max_combo = 0;
   score = 0;
 
   int menu_state = 1;
@@ -44,6 +47,10 @@ int main() {
       printf("score v2 (current: %d) ", score_v2);
       scanf("%d", &score_v2);
       continue;
+    case '3':
+      printf("score v3 (current: %d) ", score_v3);
+      scanf("%d", &score_v3);
+      continue;
     case 'x':
       menu_state = 0;
       break;
@@ -69,31 +76,53 @@ int main() {
     
     if (num != input_num) {
       if (lives - 1 <= 0) {
+	if (combo > max_combo) {
+	  max_combo = combo;
+	  combo = 0;
+	}
 	break;
       }
       lives = lives - 1;
-      combo = 0;
-      printf(" -1");
-      Sleep(750);
+      if (score_v2 != 0 || score_v3 != 0) {
+	combo = 0;
+	printf(" skill issue");
+      }
+      Sleep(450);
       continue;
     }
     else {
-      if (score > end_score && end_score != -1) {
-	score = end_score;
-	lives = 0;
-      }
       if (score_v2 != 0) {
-	if (1650 - (end - begin) > 0) {
-	  score += 1650 - (int)(end - begin);
+	if (1875 - (end - begin) > 0) {
+	  score += 1875 - (int)(end - begin);
 	  combo++;
-	  printf(" +1");
+	  printf(" very good >_<");
+	}
+      }
+      else if (score_v3 != 0) {
+	if (2125 - (end - begin) > 0) {
+	  score += (2125 - (int)(end - begin)) * ((combo / 100) + 1) * ((num / 100) + 1);
+	  combo++;
+	  printf(" best >:3");
+	}
+	else {
+	  if (combo > max_combo) {
+	    max_combo = combo;
+	  }
+
+	  combo = 0;
+	  printf(" mid :/");
 	}
       }
       else {
 	 score++;
       }
+
+      if (score > end_score && end_score != -1) {
+	score = end_score;
+	lives = 0;
+      }
     }
-    Sleep(750);
+    Sleep(350);
   }
 
   if (record_time != 0) {
@@ -101,7 +130,18 @@ int main() {
   }
 
   printf("\a");
-  printf("score: %d  time: %.2fs  combo: %d\n", score, (float)(end_time - start_time) / CLOCKS_PER_SEC, combo);
+  printf("score: %d\n", score);
+  if (record_time != 0) {
+    printf("time: %.2fs\n", (float)(end_time - start_time) / CLOCKS_PER_SEC);
+  }
+  if (combo + max_combo > 0) {
+    if (score_v3 != 0) {
+      printf("max/last combo: %d/%d\n", max_combo, combo);
+    }
+    else {
+      printf("combo: %d\n", combo);
+    }
+  }
   system("pause");
   
   return 0;
