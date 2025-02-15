e09cc8d4c3933dbf477c77e0ce9f06c59684f4d8
kynlux <kynlux@yandex.com>
2025-02-15 14:47:13 +0500

    v1.1.0

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-from os import system
+from os import system, mkdir, listdir
+from os.path import isdir, isfile, join
 from time import time
 
 class Data_Handler:
     def __init__(self):
-        self.charts = [
-            {"name": "funny test", 
-             "difficulty": 1, 
-             "patterns": ["test test test test"] * 2},
-            
-            {"name": "first map ong", 
-             "difficulty": 8, 
-             "patterns": [
-                 "first map ong",
-                 "imagine super long word",
-                 "do you got hippopotomonstrosesquipedaliophobia?",
-                 ":)",
-                 "1 2 3 4 5 6 7 8 9 0",
-                 "qwerty yuiop",
-                 "pretty fun chart"
-                 ]},
-        ]
+        self.charts = []
+        
+    def init_maps(self, user):
+        if isdir("./charts"):
+            charts = [f for f in listdir("./charts") if isfile(join("./charts", f))]
+            for chart in charts:
+                if isfile("./charts/" + chart):
+                    chart_file = open("./charts/" + chart)
+                    
+                    chart_info = {"name": "", "difficulty": -1, "patterns": []}
+                    info_collected = 0
+                    for chart_line in chart_file:
+                        if info_collected != 2:
+                            info_line = chart_line.strip().split(" : ")
+                            if info_line:
+                                if info_line[0] == "display":
+                                    info_collected += 1
+                                    chart_info["name"] = info_line[1]
+                                elif info_line[0] == "diff":
+                                    info_collected += 1
+                                    chart_info["difficulty"] = info_line[1]
+                        else:
+                            if chart_line.strip() != "":
+                                chart_info["patterns"].append(chart_line.strip())
+                    
+                    self.charts.append(chart_info)
+                    
+                    chart_file.close()
+        else:
+            mkdir("./charts")
+            user.leave("your game has initialized, download base pack of maps (zip in last release) to play\n")
 
     def get_chart(self, name):
         for chart in self.charts:
@@ -37,6 +52,12 @@ class User:
 
     def clear_console(self):
         system("cls")
+        
+    def leave(self, reason = ""):
+        self.clear_console()
+        print(reason + "see you next time\n")
+        input("(press Enter to exit)\n")
+        exit()
 
     def set_best(self, chart, time, wpm, score):
         if time == -1 or wpm == -1:
@@ -51,17 +72,22 @@ class User:
         game_started = False
         while not game_started:
             self.clear_console()
-            print(" - {} ({})".format(data.charts[self.menu_ptr]["name"], data.charts[self.menu_ptr]["difficulty"]))
-            menu_action = input(">> ")
-            if menu_action.lower() == "prev":
-                if len(data.charts) - 1 > self.menu_ptr:
-                    self.menu_ptr += 1
-            elif menu_action.lower() == "next":
-                if len(data.charts) > abs(self.menu_ptr):
-                    self.menu_ptr -= 1
-            elif menu_action.lower() == "play":
-                game_started = True
-                game.start_chart(data.charts[self.menu_ptr]["name"], data, self)      
+            if data.charts != []:
+                print(" - {} ({})".format(data.charts[self.menu_ptr]["name"], data.charts[self.menu_ptr]["difficulty"]))
+                menu_action = input(">> ")
+                if menu_action.lower() == "prev":
+                    if len(data.charts) - 1 > self.menu_ptr:
+                        self.menu_ptr += 1
+                elif menu_action.lower() == "next":
+                    if len(data.charts) > abs(self.menu_ptr):
+                        self.menu_ptr -= 1
+                elif menu_action.lower() == "play":
+                    game_started = True
+                    game.start_chart(data.charts[self.menu_ptr]["name"], data, self)      
+                elif menu_action.lower() == "leave":
+                    self.leave()
+            else:
+                self.leave("no initial songs! please donwload them from last github release and extract to ./charts folder!\n")
 
 class Game:
     def __init__(self):
@@ -120,7 +146,7 @@ class Game:
             else:
                 print("you lose!")
             
-            input("press Enter to continue")
+            input("(press Enter to continue)")
             user.display_menu(self, data)
             
             self.current_chart["name"] = ""
@@ -135,6 +161,7 @@ def main():
     data = Data_Handler()
     user = User()
     game = Game()
+    data.init_maps(user)
     user.display_menu(game, data)
 
 if __name__ == "__main__":
